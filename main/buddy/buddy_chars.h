74778931b2aaf018ef26f7d6ffe2316fa07394b1
Myfun <myfung@163.com>
2026-05-14 15:14:58 +0800

    refactor: 新角色数据结构 - 5行12列帧 + SEQ动画序列

--- main/buddy/buddy_chars.h
+++ main/buddy/buddy_chars.h
@@ -2,9 +2,11 @@
 
 #include <stdint.h>
 
-#define MAX_ANIM_FRAMES  4
-#define BUDDY_STATE_COUNT 7
-#define BUDDY_FRAME_LINES 12
+#define BUDDY_STATE_COUNT  7
+#define BUDDY_FRAME_LINES  5
+#define BUDDY_FRAME_COLS   12
+#define MAX_ANIM_FRAMES   10
+#define MAX_SEQ_LEN       32
 
 typedef enum {
     BUDDY_SLEEP = 0,
@@ -18,9 +20,11 @@ typedef enum {
 
 typedef struct {
     const char *name;
-    const char *personality;
+    uint16_t body_color;
     const char *const (*state_frames[BUDDY_STATE_COUNT])[MAX_ANIM_FRAMES][BUDDY_FRAME_LINES];
-    uint8_t frame_count[BUDDY_STATE_COUNT];
+    const uint8_t *seq[BUDDY_STATE_COUNT];       // SEQ array: tick -> pose index
+    uint8_t seq_len[BUDDY_STATE_COUNT];           // SEQ array length
+    uint8_t pose_count[BUDDY_STATE_COUNT];        // number of unique poses
 } buddy_species_t;
 
 extern const buddy_species_t BUDDY_SPECIES[];
