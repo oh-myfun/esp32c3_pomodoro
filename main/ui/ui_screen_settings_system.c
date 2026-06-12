53ed08a57f7135df9fe42166093e083afc984e83
Myfun <myfung@163.com>
2026-06-12 11:00:01 +0800

    refactor: 系统设置页移除 Sound 项（4→3 项）

--- main/ui/ui_screen_settings_system.c
+++ main/ui/ui_screen_settings_system.c
@@ -4,20 +4,20 @@
 #include "ui_manager.h"
 #include "ui_list.h"
 #include "service/storage_service.h"
-#include "service/sound_service.h"
 #include "input/input_handler.h"
 #include "esp_log.h"
 #include <stdio.h>
 
 static const char *TAG = "UI_SETTINGS_SYSTEM";
 
-#define SYSTEM_ITEM_COUNT 4
+#define SYSTEM_ITEM_COUNT 3
 
 typedef enum { MODE_SELECT, MODE_ADJUST } sys_mode_t;
 
 static sys_mode_t sys_mode = MODE_SELECT;
 static int system_selected_item = 0;
-static int system_values[SYSTEM_ITEM_COUNT] = {1, 0, 0, 1};  /* sound, dir, lang, sleep_idx */
+/* system_values[0..2]: dir, lang, sleep_idx（sound 已移到独立页） */
+static int system_values[SYSTEM_ITEM_COUNT] = {0, 0, 1};
 static const int sleep_mins[] = {0, -10, -30, 1, 2, 5, 10};  /* negative = seconds */
 #define SLEEP_OPT_COUNT (sizeof(sleep_mins) / sizeof(sleep_mins[0]))
 
@@ -31,29 +31,25 @@ static ui_list_item_t items[SYSTEM_ITEM_COUNT];
 
 static void update_display(void)
 {
-    const char *on_off[] = {i18n(STR_OFF), i18n(STR_ON)};
     const char *dir_opts[] = {i18n(STR_NORMAL), i18n(STR_REV)};
     const char *lang_opts[] = {i18n(STR_LANG_EN), i18n(STR_LANG_ZH)};
 
-    snprintf(item_keys[0], sizeof(item_keys[0]), "%s", i18n(STR_SOUND));
-    snprintf(item_values[0], sizeof(item_values[0]), "%s", on_off[system_values[0] % 2]);
+    snprintf(item_keys[0], sizeof(item_keys[0]), "%s", i18n(STR_DIRECTION));
+    snprintf(item_values[0], sizeof(item_values[0]), "%s", dir_opts[system_values[0] % 2]);
 
-    snprintf(item_keys[1], sizeof(item_keys[1]), "%s", i18n(STR_DIRECTION));
-    snprintf(item_values[1], sizeof(item_values[1]), "%s", dir_opts[system_values[1] % 2]);
+    snprintf(item_keys[1], sizeof(item_keys[1]), "%s", i18n(STR_LANGUAGE));
+    snprintf(item_values[1], sizeof(item_values[1]), "%s", lang_opts[system_values[1] % 2]);
 
-    snprintf(item_keys[2], sizeof(item_keys[2]), "%s", i18n(STR_LANGUAGE));
-    snprintf(item_values[2], sizeof(item_values[2]), "%s", lang_opts[system_values[2] % 2]);
-
-    snprintf(item_keys[3], sizeof(item_keys[3]), "%s", i18n(STR_SLEEP_TIMEOUT));
+    snprintf(item_keys[2], sizeof(item_keys[2]), "%s", i18n(STR_SLEEP_TIMEOUT));
     {
-        int idx = system_values[3] % SLEEP_OPT_COUNT;
+        int idx = system_values[2] % SLEEP_OPT_COUNT;
         int val = sleep_mins[idx];
         if (val == 0) {
-            snprintf(item_values[3], sizeof(item_values[3]), "%s", i18n(STR_OFF_VAL));
+            snprintf(item_values[2], sizeof(item_values[2]), "%s", i18n(STR_OFF_VAL));
         } else if (val < 0) {
-            snprintf(item_values[3], sizeof(item_values[3]), i18n(STR_FMT_SEC), -val);
+            snprintf(item_values[2], sizeof(item_values[2]), i18n(STR_FMT_SEC), -val);
         } else {
-            snprintf(item_values[3], sizeof(item_values[3]), i18n(STR_FMT_MIN), val);
+            snprintf(item_values[2], sizeof(item_values[2]), i18n(STR_FMT_MIN), val);
         }
     }
 
@@ -81,8 +77,8 @@ static void update_display(void)
 static void system_on_encoder_cw(void)
 {
     if (sys_mode == MODE_ADJUST) {
-        if (system_selected_item == 3) {
-            system_values[3] = (system_values[3] + 1) % SLEEP_OPT_COUNT;
+        if (system_selected_item == 2) {
+            system_values[2] = (system_values[2] + 1) % SLEEP_OPT_COUNT;
         }
         update_display();
     } else {
@@ -94,8 +90,8 @@ static void system_on_encoder_cw(void)
 static void system_on_encoder_ccw(void)
 {
     if (sys_mode == MODE_ADJUST) {
-        if (system_selected_item == 3) {
-            system_values[3] = (system_values[3] - 1 + SLEEP_OPT_COUNT) % SLEEP_OPT_COUNT;
+        if (system_selected_item == 2) {
+            system_values[2] = (system_values[2] - 1 + SLEEP_OPT_COUNT) % SLEEP_OPT_COUNT;
         }
         update_display();
     } else {
@@ -110,7 +106,7 @@ static void system_on_encoder_press(void)
         /* Cancel: reload saved value */
         int32_t val;
         if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, &val) && val >= 0 && val < (int)SLEEP_OPT_COUNT) {
-            system_values[3] = (int)val;
+            system_values[2] = (int)val;
         }
         sys_mode = MODE_SELECT;
     } else {
@@ -124,10 +120,10 @@ static void system_on_settings_press(void)
 {
     if (sys_mode == MODE_ADJUST) {
         /* Save and exit adjust */
-        if (system_selected_item == 3) {
+        if (system_selected_item == 2) {
             extern int sleep_timeout_idx;
-            sleep_timeout_idx = system_values[3];
-            storage_save_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, system_values[3]);
+            sleep_timeout_idx = system_values[2];
+            storage_save_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, system_values[2]);
         }
         sys_mode = MODE_SELECT;
         update_display();
@@ -137,37 +133,18 @@ static void system_on_settings_press(void)
     switch (system_selected_item) {
         case 0:
             system_values[0] = !system_values[0];
-            sound_service_set_enabled(system_values[0]);
-            storage_save_int(STORAGE_NAMESPACE_SETTINGS, KEY_SOUND, system_values[0]);
+            input_handler_set_reverse(system_values[0]);
+            storage_save_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, system_values[0]);
             break;
         case 1:
             system_values[1] = !system_values[1];
-            input_handler_set_reverse(system_values[1]);
-            storage_save_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, system_values[1]);
+            i18n_set_lang(system_values[1] ? LANG_ZH : LANG_EN);
             break;
         case 2:
-            system_values[2] = !system_values[2];
-            i18n_set_lang(system_values[2] ? LANG_ZH : LANG_EN);
-            break;
-        case 3:
             sys_mode = MODE_ADJUST;
             break;
     }
     update_display();
-}
-
-static void system_on_encoder_long_press(void)
-{
-    if (sys_mode == MODE_ADJUST) {
-        int32_t val;
-        if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, &val) && val >= 0 && val < (int)SLEEP_OPT_COUNT) {
-            system_values[3] = (int)val;
-        }
-        sys_mode = MODE_SELECT;
-        update_display();
-    } else {
-        ui_go_back();
-    }
 }
 
 lv_obj_t* ui_screen_settings_system_create(void)
@@ -190,17 +167,14 @@ lv_obj_t* ui_screen_settings_system_create(void)
     system_list = ui_list_create(screen, 220, 196, 10, 30);
 
     int32_t val;
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SOUND, &val)) {
+    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, &val)) {
         system_values[0] = (int)val;
     }
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, &val)) {
+    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_LANG, &val)) {
         system_values[1] = (int)val;
     }
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_LANG, &val)) {
-        system_values[2] = (int)val;
-    }
     if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, &val) && val >= 0 && val < (int)SLEEP_OPT_COUNT) {
-        system_values[3] = (int)val;
+        system_values[2] = (int)val;
     }
 
     system_selected_item = 0;
@@ -216,7 +190,7 @@ lv_obj_t* ui_screen_settings_system_create(void)
         .on_encoder_cw = system_on_encoder_cw,
         .on_encoder_ccw = system_on_encoder_ccw,
         .on_encoder_press = system_on_encoder_press,
-        .on_encoder_long_press = system_on_encoder_long_press,
+        .on_encoder_long_press = NULL,
         .on_settings_press = system_on_settings_press,
     };
     ui_register_input_callbacks(UI_SCREEN_SETTINGS_SYSTEM, &cbs);
@@ -228,17 +202,14 @@ lv_obj_t* ui_screen_settings_system_create(void)
 void ui_screen_settings_system_refresh(void)
 {
     int32_t val;
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SOUND, &val)) {
+    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, &val)) {
         system_values[0] = (int)val;
     }
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_ENC_DIR, &val)) {
+    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_LANG, &val)) {
         system_values[1] = (int)val;
     }
-    if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_LANG, &val)) {
-        system_values[2] = (int)val;
-    }
     if (storage_load_int(STORAGE_NAMESPACE_SETTINGS, KEY_SLEEP_TIMEOUT, &val) && val >= 0 && val < (int)SLEEP_OPT_COUNT) {
-        system_values[3] = (int)val;
+        system_values[2] = (int)val;
     }
     sys_mode = MODE_SELECT;
     update_display();
